4b36a5d5d88dda44c246ae25a259742e287eaddc
Alison C <alizoncaetano@gmail.com>
2025-02-19 20:21:21 -0300

    teste página em construção v0.1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,28 @@
+import styled from 'styled-components'
+
 function Home() {
-  return <h1>( ͡ಥ ͜ʖ ͡ಥ)</h1>;
+  return (
+  <Container>
+    <h1>Criando algo novo...</h1>
+  </Container>
+  )  
 }
+
+const Container = styled.div`
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+
+  display: grid;
+  height: 95vh;
+  width: 100%;
+  place-content: center;
+ 
+  h1 {
+    color: #222;
+    font-size: 2rem;
+    font-family: "Space Mono", monospace;
+  }
+`
 
 export default Home;
