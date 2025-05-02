a67cb96fffb6c0eb6b397520a5c0b3b5fe1831b2
Alison C <alizoncaetano@gmail.com>
2025-05-01 23:03:40 -0300

    create `database` module

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,10 +1,14 @@
+import database from "../../../../infra/database.ts";
 import { NextApiRequest, NextApiResponse } from "next";
 
 type StatusResponse = {
   chave: String;
 };
 
-function status(request: NextApiRequest, response: NextApiResponse<StatusResponse>): void {
+async function status(request: NextApiRequest, response: NextApiResponse<StatusResponse>): void {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
+  
   response.status(200).json({ chave: "valor" });
 }
 
