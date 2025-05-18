8d3d31fe387bcd323a249dff8d570a46dd895276
Alison C <alizoncaetano@gmail.com>
2025-05-18 20:14:21 -0300

    add: assertions for `dbVersion`, `dbMaxConn`, `dpOpenedConn`

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,15 +1,18 @@
 import database from "infra/database.ts";
 import { NextApiRequest, NextApiResponse } from "next";
 
-type StatusResponse = {
-  chave: String;
-};
+async function status(request: NextApiRequest, response: NextApiResponse): Promise<void> {
+  const updatedAt = new Date().toISOString();
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbMaxConn = await database.query("SHOW max_connections;");
+  const dbOpenedConn = await database.query("SELECT count(*) FROM pg_stat_activity;");
 
-async function status(request: NextApiRequest, response: NextApiResponse<StatusResponse>): Promise<void> {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  
-  response.status(200).json({ chave: "valor" });
+  response.status(200).json({
+    update_at: updatedAt,
+    db_version: dbVersion,
+    db_max_conn: dbMaxConn,
+    db_opened_conn: dbOpenedConn,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,4 +1,19 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+
+  const dbVersion = responseBody.db_version.rows[0].server_version;
+  expect(dbVersion).toEqual("16.0");
+
+  const dbMaxConn = responseBody.db_max_conn.rows[0].max_connections;
+  expect(dbMaxConn).toEqual("100");
+
+  const dbOpenedConn = responseBody.db_opened_conn.rows[0].count;
+  expect(parseInt(dbOpenedConn)).toBeGreaterThan(0);
 });
