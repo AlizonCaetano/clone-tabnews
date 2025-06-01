63b8000518232ec95916e300fee447b51e606bf1
Alison C <alizoncaetano@gmail.com>
2025-05-31 22:12:50 -0300

    add query using node-pg sanitization

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -13,10 +13,12 @@ async function status(
   const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
   const { max_connections } = dbMaxConnectionsResult.rows[0];
 
-  const dbOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity;");
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname= $1;",
+    values: [dbName]
+  });
   const { count } = dbOpenedConnectionsResult.rows[0];
-
-  console.log(count);
 
   response.status(200).json({
     update_at: updatedAt,
